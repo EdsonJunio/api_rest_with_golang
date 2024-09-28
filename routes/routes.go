894f579d75cf06537064964b25ad106a72fe9487
edson.ribeiro_onfly <edson.ribeiro@onfly.com.br>
2024-09-28 11:45:03 -0300

    go

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"api_rest_with_golang/controllers"
+	"api_rest_with_golang/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -9,8 +11,12 @@ import (
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornarUmaPersonalidades).Methods("Get")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalidades", controllers.CriarPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletarPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditarPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
